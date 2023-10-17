f92cb05f667f5562c5d8a65849948525f4132ea0
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-10-16 23:10:02 -0400

    update in services with products get all, get one, delete and create product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -24,19 +24,26 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    const products = this.productRepository.find();
+    return products
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id)
+    await this.productRepository.remove(product)
+    return {message : `product whit id ${id} removed successfully`}
   }
 
   private handleDbExceptions(error: any) {
