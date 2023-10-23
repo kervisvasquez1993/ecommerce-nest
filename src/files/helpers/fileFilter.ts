f8be2793056a3f52c7f4924711116761feb0ebdc
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-10-23 14:38:30 -0400

    upload file controller

---
+++ src/files/helpers/fileFilter.ts
@@ -0,0 +1,16 @@
+
+
+export const fileFilter = (req: Express.Request, file: Express.Multer.File, callback: Function) => {
+    console.log({file})
+    if(!file) return callback(new Error("File is empty"), false);
+
+    const fileExptension = file.mimetype.split("/")[1];
+    const validExptension = ["jpg", "png", "png", "gif"]
+
+    if(validExptension.includes(fileExptension)){
+        return callback(null, true)
+    }
+
+    return callback(null, false)
+   
+}
