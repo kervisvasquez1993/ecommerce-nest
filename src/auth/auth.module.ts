d4792ae3d2d2d85ddd1ee64b8892acb458953fa0
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-10-24 15:53:13 -0400

    add jwd in config init

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,37 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]),
+  PassportModule.register({
+    defaultStrategy: "jwt"
+  }),
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => {
+      // console.log("jwt secret", configService.get("JWTSECRET"))
+      return {
+        secret: configService.get("JWTSECRET"),
+        signOptions: {
+          expiresIn: "2h"
+        }
+      }
+    }
+  })
+    // JwtModule.register({
+    //   secret: process.env.JSWSECRET,
+    //   signOptions : {
+    //     expiresIn : "2h"
+    //   }
+    // })
+  ],
   exports: [TypeOrmModule]
 
 })
