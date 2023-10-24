c4ef52896c0b2a4424fa628943cdcdb91514ea0c
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-10-23 22:20:27 -0400

    add dto for create user and add router register

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+    @Column("text", {
+        unique: true
+    })
+    email: string;
+    @Column("text")
+    password: string;
+    @Column("text")
+    fullName: string;
+    @Column("bool")
+    isActive: boolean;
+    @Column("text", {
+        array: true,
+        default: []
+    })
+    roles: string[]
+}
